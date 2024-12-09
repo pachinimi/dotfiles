16938e3d4eee386a89a75e3faab85679a3d2b77b
pachi <pachinimi@gmail.com>
2024-12-09 20:46:16 -0300

    Update network.nix

--- hosts/gaia/system/modules/network.nix
+++ hosts/gaia/system/modules/network.nix
@@ -5,7 +5,6 @@
   networking = {
 
     hostName = "gaia";
-    firewall.enable = true;
     nameservers = [
       "127.0.0.1"
       "::1"
@@ -31,54 +30,30 @@
     StateDirectory = "dnscrypt-proxy";
   };
 
-  # Set a static IP on the "downstream" interface
-networking.interfaces."enp1s0" = {
-  useDHCP = false;
-  ipv4.addresses = [{
-    address = "10.0.0.1";
-    prefixLength = 24;
-  }];
-};
-networking.firewall.extraCommands = ''
-  # Set up SNAT on packets going from downstream to the wider internet
-  iptables -t nat -A POSTROUTING -o wlp2s0 -j MASQUERADE
-
-  # Accept all connections from downstream. May not be necessary
-  iptables -A INPUT -i enp1s0 -j ACCEPT
+networking.firewall.enable = false;
+networking.nftables.enable = true;
+networking.nftables.ruleset = ''
+   table inet firewall {
+       chain incoming {
+         type filter hook input priority 0; policy drop;
+         
+         ct state vmap { established : accept, related : accept, invalid : drop }
+         
+         # Allow SSH
+         tcp dport 22 ct state new tcp flags & (syn | ack) == syn counter accept
+       }
+       
+       chain forward {
+         type filter hook forward priority 0; policy accept;
+       }
+   }
+       
+   table ip router {
+        chain postrouting {
+          type nat hook postrouting priority srcnat; policy accept;
+          oifname "enp1s0" counter masquerade
+        }
+   }
 '';
-# Run a DHCP server on the downstream interface
-services.kea.dhcp4 = {
-  enable = true;
-  settings = {
-    interfaces-config = {
-      interfaces = [
-        "enp1s0"
-      ];
-    };
-    lease-database = {
-      name = "/var/lib/kea/dhcp4.leases";
-      persist = true;
-      type = "memfile";
-    };
-    rebind-timer = 2000;
-    renew-timer = 1000;
-    subnet4 = [
-      {
-        id = 1;
-        pools = [
-          {
-            pool = "10.0.0.2 - 10.0.0.255";
-          }
-        ];
-        subnet = "10.0.0.1/24";
-      }
-    ];
-    valid-lifetime = 4000;
-    option-data = [{
-      name = "routers";
-      data = "10.0.0.1";
-    }];
-  };
-};
 
 }
