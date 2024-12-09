66e4491e1f131ffcc47f516c6aad56a0a00e04dd
pachi <pachinimi@gmail.com>
2024-12-09 19:21:30 -0300

    Update network.nix

--- hosts/gaia/system/modules/network.nix
+++ hosts/gaia/system/modules/network.nix
@@ -31,17 +31,55 @@
     StateDirectory = "dnscrypt-proxy";
   };
 
-  services.openssh = {
-    enable = true;
-    openFirewall = true;
-    settings = {
-      PasswordAuthentication = true;
-      AllowUsers = [ "pachi" ];
-      X11Forwarding = false;
-      PermitRootLogin = "no";
-    };
-  };
+  # Set a static IP on the "downstream" interface
+networking.interfaces."eth0" = {
+  useDHCP = false;
+  ipv4.addresses = [{
+    address = "10.0.0.1";
+    prefixLength = 24;
+  }];
+};
+networking.firewall.extraCommands = ''
+  # Set up SNAT on packets going from downstream to the wider internet
+  iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 
-  services.fail2ban.enable = true;
+  # Accept all connections from downstream. May not be necessary
+  iptables -A INPUT -i enp2s0 -j ACCEPT
+'';
+# Run a DHCP server on the downstream interface
+services.kea.dhcp4 = {
+  enable = true;
+  settings = {
+    interfaces-config = {
+      interfaces = [
+        "eth0"
+      ];
+    };
+    lease-database = {
+      name = "/var/lib/kea/dhcp4.leases";
+      persist = true;
+      type = "memfile";
+    };
+    rebind-timer = 2000;
+    renew-timer = 1000;
+    subnet4 = [
+      {
+        id = 1;
+        pools = [
+          {
+            pool = "10.0.0.2 - 10.0.0.255";
+          }
+        ];
+        subnet = "10.0.0.1/24";
+      }
+    ];
+    valid-lifetime = 4000;
+    option-data = [{
+      name = "routers";
+      data = "10.0.0.1";
+    }];
+  };
+};
+
 
 }
