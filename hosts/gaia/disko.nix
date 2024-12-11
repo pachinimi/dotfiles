398ad3359c8dcfb13bc96e2ba881a06ea9a1c577
pachi <pachinimi@gmail.com>
2024-12-11 17:39:47 -0300

    modified:   gaia/disko.nix 	modified:   navi/disko.nix

--- hosts/gaia/disko.nix
+++ hosts/gaia/disko.nix
@@ -1,59 +1,61 @@
 { ... }:
 
 {
-  disk = {
-    main = {
-      content = {
-        type = "gpt";
-        partitions = {
-          grub = {
-            size = "1M";
-            type = "EF02";
-          };
-          boot = {
-            size = "512M";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+  disko.devices = {
+    disk = {
+      main = {
+        content = {
+          type = "gpt";
+          partitions = {
+            grub = {
+              size = "1M";
+              type = "EF02";
             };
-          };
-          luks = {
-            size = "100%";
-            content = {
-              type = "luks";
-              name = "crypted";
-              settings = {
-                allowDiscards = true;
-              };
+            boot = {
+              size = "512M";
               content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "/root" = {
-                    mountpoint = "/";
-                    mountOptions = [
-                      "compress-force=zstd"
-                      "noatime"
-                    ];
-                  };
-                  "/home" = {
-                    mountpoint = "/home";
-                    mountOptions = [
-                      "compress-force=zstd"
-                      "noatime"
-                    ];
-                  };
-                  "/nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [
-                      "compress-force=zstd"
-                      "noatime"
-                    ];
-                  };
-                  "/swap" = {
-                    mountpoint = "/.swapvol";
-                    swap.swapfile.size = "20M";
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                settings = {
+                  allowDiscards = true;
+                };
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress-force=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "compress-force=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress-force=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/swap" = {
+                      mountpoint = "/.swapvol";
+                      swap.swapfile.size = "20M";
+                    };
                   };
                 };
               };
--- hosts/navi/disko.nix
+++ hosts/navi/disko.nix
@@ -4,7 +4,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/vdb";
+        device = "/dev/sda";
         content = {
           type = "gpt";
           partitions = {
