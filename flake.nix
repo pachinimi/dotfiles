1d07a082881906f44e073eea34004435a7a56df9
pachi <pachinimi@gmail.com>
2024-12-10 22:23:47 -0300

    modified:   flake.nix 	deleted:    hosts/gaia/disko.nix 	deleted:    hosts/navi/disko.nix

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.1";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -34,6 +38,73 @@
           modules = [
             ./hosts/navi/system
             lanzaboote.nixosModules.lanzaboote
+            disko.nixosModules.disko
+            {
+              disko.devices = {
+                disk = {
+                  main = {
+                    type = "disk";
+                    device = "/dev/vdb";
+                    content = {
+                      type = "gpt";
+                      partitions = {
+                        ESP = {
+                          size = "512M";
+                          type = "EF00";
+                          content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                            mountOptions = [ "umask=0077" ];
+                          };
+                        };
+                        luks = {
+                          size = "100%";
+                          content = {
+                            type = "luks";
+                            name = "crypted";
+                            settings = {
+                              allowDiscards = true;
+                            };
+                            content = {
+                              type = "btrfs";
+                              extraArgs = [ "-f" ];
+                              subvolumes = {
+                                "/root" = {
+                                  mountpoint = "/";
+                                  mountOptions = [
+                                    "compress-force=zstd"
+                                    "noatime"
+                                  ];
+                                };
+                                "/home" = {
+                                  mountpoint = "/home";
+                                  mountOptions = [
+                                    "compress-force=zstd"
+                                    "noatime"
+                                  ];
+                                };
+                                "/nix" = {
+                                  mountpoint = "/nix";
+                                  mountOptions = [
+                                    "compress-force=zstd"
+                                    "noatime"
+                                  ];
+                                };
+                                "/swap" = {
+                                  mountpoint = "/.swapvol";
+                                  swap.swapfile.size = "20M";
+                                };
+                              };
+                            };
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+            }
             home-manager.nixosModules.home-manager
             {
               home-manager.extraSpecialArgs = { inherit inputs; };
